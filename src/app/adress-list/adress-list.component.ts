7c018141df30d4e0c0a04e614bc5af8cacbba6e9
hacene <38687021+hacene1986@users.noreply.github.com>
2022-09-27 23:15:36 +0200

    commit app

--- src/app/adress-list/adress-list.component.ts
+++ src/app/adress-list/adress-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalFormComponent } from '../modal-form/modal-form.component';
+import { Adresse } from '../models/adresse';
+import { AdresseService } from '../services/adresse.service';
 
 declare var window: any;
 @Component({
@@ -8,20 +11,54 @@ declare var window: any;
 })
 export class AdressListComponent implements OnInit {
 
+  @ViewChild(ModalFormComponent) addview !:ModalFormComponent
   formModal: any;
-  constructor() { }
+  contactAdresses!: Adresse[];
+  
+  constructor(
+    public adressService: AdresseService,
+  ) { }
 
   ngOnInit(): void {
     this.formModal = new window.bootstrap.Modal(
       document.getElementById("exampleModal")
     );
+
+    this.getAllAdresse();
   }
 
+  // Open Modal
   openModal(){
+    this.addview.Clearform();
     this.formModal.show();
   }
 
-  doSomething(){
-    this.formModal.hide();
+  //Get all contacts
+  getAllAdresse(){
+   this.adressService.getAdresses().subscribe(res => {
+     this.contactAdresses = res.map(e => {
+       return {
+         id: e.payload.doc.id,
+         ...e.payload.doc.data() as {}
+       } as Adresse
+     })
+     console.log(this.contactAdresses)
+   });
+   
   }
+
+  //Remove contact
+  removeContact(Adresse: Adresse){
+    if(confirm("Vous etes sur de vouloir supprimer " + Adresse.nom)){
+      this.adressService.deleteAdress(Adresse);
+    }
+  }
+
+  edit(id: any){
+   this.addview.LoadEditData(id);
+  }
+
+
+
+  
 }
--- src/app/modal-form/modal-form.component.ts
+++ src/app/modal-form/modal-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AdresseService } from '../services/adresse.service';
 
+
+declare var window: any;
 @Component({
   selector: 'app-modal-form',
   templateUrl: './modal-form.component.html',
@@ -9,25 +11,108 @@ import { AdresseService } from '../services/adresse.service';
 })
 export class ModalFormComponent implements OnInit {
 
-  public contactForm: FormGroup;
+  formModal: any;
+  contactForm: any;
+  errorMessage!: string;
+  contact: any;
   constructor(
     public adressService: AdresseService,
     public formBuilder: FormBuilder,
   ) { 
-    this.contactForm = this.formBuilder.group({
-      nom: [''],
-      prenom: [''],
-      email: [''],
-      tel: ['']
-    })
+    
   }
 
   ngOnInit(): void {
+
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById("exampleModal")
+    );
+    
+    this.contactForm = new FormGroup({
+      nom: new FormControl('', Validators.compose([Validators.required])),
+      prenom: new FormControl('', Validators.compose([Validators.required])),
+      email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+
+      tel: new FormArray([
+        new FormControl(null, Validators.compose([Validators.required])),
+       
+      ])
+    })
+
+  
+  }
+
+  get nom(){
+    return this.contactForm.get('nom');
+  }
+
+  get prenom(){
+    return this.contactForm.get('prenom');
+  }
+
+  get email(){
+    return this.contactForm.get('email');
+  }
+
+  get tel(){
+    return this.contactForm.get('tel');
+  }
+
+
+
+
+  close(){
+    console.log('ok')
+    this.formModal.hide();
+  }
+
+  LoadEditData(id: any){
+    localStorage.setItem('ID', id);
+    this.formModal.show();
+    this.adressService.getAdressById(id).subscribe(res => {
+      console.log(res);
+      this.contact = res;
+      this.contactForm.setValue({
+        nom: this.contact.nom,
+        prenom: this.contact.prenom,
+        email: this.contact.email,
+        tel : this.contact.tel
+        // new FormArray([
+        //   this.contact.tel.forEach((element: any) => {
+        //     console.log("ELEMENT====", element);
+            
+        //     new FormControl(element, Validators.compose([Validators.required]))
+        //   })
+        // ])
+      })
+    })
+  }
+
+  addTel(){
+    const control=new FormControl(null,Validators.required);
+    (<FormArray>this.contactForm.get('tel')).push(control);
+  }
+
+  Clearform(){
+    this.ngOnInit();
   }
 
   onSubmit(){
     console.log(this.contactForm.value)
-   // this.adressService.createAdress(this.contactForm.value);
+    if(this.contactForm.value.nom != "" && this.contactForm.value.prenom != "" && this.contactForm.value.emain != "" && this.contactForm.value.tel){
+      console.log(this.contact)
+      console.log(localStorage.getItem("ID"));
+      
+      this.adressService.updateAdress(this.contactForm.value, localStorage.getItem("ID"))
+    
+      return
+    }
+    if(this.contactForm.valid){
+      this.adressService.createAdress(this.contactForm.value);
+    }else{
+      this.errorMessage = "Remplissez le formulaire"
+    }
+   
   }
 
 }
